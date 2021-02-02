ec7c7fceb5f02d48a932ef7f57c60b883623fc07
Jessie Auguste <jessieauguste@gmail.com>
2021-02-02 19:27:39 +0000

    added contact us page

--- ajhomepage.js
+++ ajhomepage.js
@@ -2,3 +2,7 @@ function myFunction() {
     document.getElementById("demo").innerHTML="Thanks for signing up!";
 
 }
+function messagePost() {
+    document.getElementById("submit").innerHTML="Thanks for contacting us, we will be in touch with you soon";
+
+}
